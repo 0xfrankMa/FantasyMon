a36d4dfaef5c1827c24fa10fd611fcb005bd417e
0xfrankMa <frank@ultiverse.io>
2026-03-02 15:27:07 -0800

    feat: route to RunRewardScreen when run is complete

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,14 @@
 // apps/web/src/App.tsx
 import React, { useState, useEffect } from 'react'
 import type { SaveFile } from '@fantasymon/core'
-import { ALL_BUFFS, getCurrentNode } from '@fantasymon/core' // ensures BUFF_REGISTRY populated before loadSave; eslint-disable-line @typescript-eslint/no-unused-vars
+import { ALL_BUFFS, getCurrentNode, isRunComplete } from '@fantasymon/core' // ensures BUFF_REGISTRY populated before loadSave; eslint-disable-line @typescript-eslint/no-unused-vars
 import { loadSave, newSave, writeSave } from './save'
 import { HomeScreen } from './screens/HomeScreen'
 import { TeamBuilderScreen } from './screens/TeamBuilderScreen'
 import { BattleScreen } from './screens/BattleScreen'
 import { ShopScreen } from './screens/ShopScreen'
 import { RestScreen } from './screens/RestScreen'
+import { RunRewardScreen } from './screens/RunRewardScreen'
 
 export type Screen = 'home' | 'teambuilder' | 'run'
 
@@ -22,6 +23,9 @@ export function App() {
   }
 
   if (screen === 'run') {
+    if (save.runState && isRunComplete(save.runState)) {
+      return <RunRewardScreen save={save} setSave={setSave} onBack={() => setScreen('home')} />
+    }
     const currentNode = save.runState ? getCurrentNode(save.runState) : null
     if (currentNode?.type === 'shop') {
       return <ShopScreen save={save} setSave={setSave} onBack={() => setScreen('home')} />
